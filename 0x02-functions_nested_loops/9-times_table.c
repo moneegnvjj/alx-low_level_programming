bbaf193b9378d6c2adb5f6a33c78ffe850a38b44
moneegnvjj <simondavid8704@gmail.com>
2023-09-13 13:58:31 -0700

    Write a function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar('i');
+				_putchar(' ');
 			}
 				_putchar((k % 10) + '0');
 			if (j < 9)
@@ -35,6 +35,6 @@ void times_table(void)
 				_putchar(' ');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
