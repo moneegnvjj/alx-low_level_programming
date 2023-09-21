789dc10ec9e7de5ff02a93d5a65c284af0ddbe0e
moneegnvjj <simondavid8704@gmail.com>
2023-09-21 00:28:57 -0700

    Write a function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @str: The input string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwyzabcdefghojklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c[i] != '\0'; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = d[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
