d224d101507a4e14a2472011bcd13571f141896a
moneegnvjj <simondavid8704@gmail.com>
2023-09-27 04:53:37 -0700

    Write a function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,25 @@
 #include <string.h>
 
 /**
+ * check_palindrome - Helper function to check if a substring is a palindrome.
+ * @s: The string to be checked.
+ * @start: The start index of the substring.
+ * @end: The end index of the substring.
+ *
+ * Return: 1 if the substring is a palindrome, 0 otherwise.
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	return (check_palindrome(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - Check if a string is a palindrome.
  * @s: The string to be checked.
  *
@@ -9,17 +28,10 @@
  */
 int is_palindrome(char *s)
 {
-	int left = 0;
-	int right = strlen(s) - 1;
+	int length = strlen(s);
 
-	while (left < right)
-	{
-		if (s[left] != s[right])
-			return (0); /* Not a palindrome */
+	if (length <= 1)
+		return (1); /* Empty string or single character is a palindrome */
 
-		left++;
-		right--;
-	}
-
-	return (1); /* It's a palindrome */
+	return (check_palindrome(s, 0, length - 1));
 }
