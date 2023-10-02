32b2406be44ec1208d4b761187a180446abaf8d6
moneegnvjj <simondavid8704@gmail.com>
2023-10-02 03:43:46 -0700

    Write a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,38 +1,32 @@
-#include <stdio.h>
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-#define INT_MAX 2147483647
-#define INT_MIN -2147483648
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (result != 0)
+		{
+			break;
+		}
+		i++;
+	}
 
-int _atoi(char *s) {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
-
-    // Skip leading whitespace
-    while (s[i] == ' ') {
-        i++;
-    }
-
-    // Check for sign
-    if (s[i] == '-') {
-		sign = -1;
-        i++;
-    } else if (s[i] == '+') {
-        i++;
-    }
-
-    /* Convert digits to integer*/
-    while (s[i] >= '0' && s[i] <= '9') {
-        int digit = s[i] - '0';
-
-        /* Check for overflow*/
-        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)) {
-            return (sign == 1) ? INT_MAX : INT_MIN;
-        }
-
-        result = result * 10 + digit;
-        i++;
-    }
-
-    return result * sign
+	return (result * sign);
+}
 
