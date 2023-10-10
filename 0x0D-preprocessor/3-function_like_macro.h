89a150775b59aa7adf53b900c1d8dff7ca75c5c5
moneegnvjj <simondavid8704@gmail.com>
2023-10-10 11:16:44 -0700

    Write a function-like macro ABS(x) that computes the absolute value of a number x

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,7 @@
-#ifndef MACROLIKE_H
-#define MACROLIKE_H
-/**
- * file: 3-function_like_macro
- *
- * @x: input value (an int)
- *
- * Desc: header file that computes absolute value
- */
-#define ABS(x) ((k) < 0 ? -(1) * (x) : (x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#endif
+#define ABS(x) ((x) < 0 ? (-1) * (x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
 
--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 #include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int i;
-    int j;
+int main(void) {
+    int a = -98;
+    int result1 = ABS(a) * 10;
+    int result2 = ABS(-a) * 10;
 
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
-    return (0);
+    printf("%d, %d\n", result1, result2);
+
+    return 0;
 }
+
